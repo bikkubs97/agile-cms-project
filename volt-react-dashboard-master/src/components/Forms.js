78025ec1016eead3afad26f3fc655ff3270cda4f
Bikku BS <bikku4444@gmail.com>
2023-05-13 15:03:19 +0530

    update

--- volt-react-dashboard-master/src/components/Forms.js
+++ volt-react-dashboard-master/src/components/Forms.js
@@ -22,7 +22,7 @@ export const GeneralInfoForm = () => {
   const handleSubmit = (event) => {
   
     event.preventDefault();
-    fetch('http://localhost:8000/data', {
+    fetch('https://cms-server-29mo.onrender.com/data', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
--- volt-react-dashboard-master/src/pages/Transactions.js
+++ volt-react-dashboard-master/src/pages/Transactions.js
@@ -5,7 +5,7 @@ export default () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/contact")
+    fetch("https://cms-server-29mo.onrender.com/contact")
       .then(response => response.json())
       .then(data => setMessages(data))
       .catch(error => console.error(error));
